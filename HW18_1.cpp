fc8c90c4edb55314128303944c938d5af527aa4c
Pletnov Denis <pletnevd732@gmail.com>
2025-06-12 15:06:50 +0300

    add Employees

--- HW18_1.cpp
+++ HW18_1.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "Employee.h"
 using namespace std;
 int main()
 {
-	
+	const int Size = 5;
+	Emloyee arr[Size]{
+		{"Pupkin", 1995, "Programmer", 10000.0, "Higher"},
+		{"Shevchenko", 2000, "Auditor", 8000.0, "Medium"},
+		{"Ukrainka", 1996, "Manager", 5000.0, "Higher"},
+		{"Lishenko", 2005, "Physicist", 3000.0, "Medium special"},
+		{"Kirichenko", 1999, "Accountant", 6000.0, "Higher"}
+	};
+
+
 }
