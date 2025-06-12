bf83cc70fcf72dd9bc329fed50f07eec288172be
Pletnov Denis <pletnevd732@gmail.com>
2025-06-12 14:55:25 +0300

    add struct Employee

---
+++ Employee.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+using namespace std;
+struct Emloyee
+{
+	char last_name[50];
+	int year_of_birth = 1900;
+	char position[65];
+	double salary = 5000.0;
+	char education[100];
+};
